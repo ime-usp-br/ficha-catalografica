a4b3d0f59926e0a6f9f310b541abfa0e0695f313
GustavoSantos25 <gustavo.war2015@gmail.com>
2022-05-05 13:35:40 -0300

    Gera tabela com quantidade de fichas por mês e por ano

--- resources/views/config/configs.blade.php
+++ resources/views/config/configs.blade.php
@@ -5,8 +5,42 @@
 <div class="container">
     <p><b>Cabeçalho e rodapé: </b> <a href="/configs/edit" class="btn btn-outline-dark">Editar</a></p>
     
+    
+    <div id="table-wrapper">
+        <div id="table-scroll">
+            <caption><b>Fichas geradas:</b></caption>
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                    <th></th>
+                    @foreach($anos as $ano)
+                       <th>{{$ano}}</th>     
+                    @endforeach
+                    <th>FICHAS GERADAS POR MÊS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i=0; $i < 12; $i++)
+                        <tr>
+                        <td><b>{{$meses[$i]}}</b></td>
+                        @foreach($anos as $ano)
+                            <td>{{$num_fichas[$ano][$i]}}</td>
+                        @endforeach
+                        <td>{{$totalPorMes[$i]}}</td>
+                        </tr>
+                    @endfor
 
-    <p><b>Número de fichas criadas: </b>{{$config->num_fichas ? $config->num_fichas : 0}}</p>
+                        <tr style="background-color: lightGray">
+                            <td><b>FICHAS GERADAS POR ANO</b></td>
+                            @foreach($anos as $ano)
+                                <td>{{$num_fichas[$ano]['total']}}</td>
+                            @endforeach
+                            <td style="background-color: gray"><b>TOTAL DE FICHAS GERADAS: {{$totalFichas}}</b></td>
+                        </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 </div>
 
